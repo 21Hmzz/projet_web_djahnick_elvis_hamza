752503ee4adb44be0abdfacc31db3bbdc697d8b9
Hamza Bella <hamza@stevegates.co>
2024-07-10 22:30:45 +0200

    update for render

--- app/src/redis/redis.module.ts
+++ app/src/redis/redis.module.ts
@@ -7,11 +7,11 @@ import Redis from 'ioredis';
     {
       provide: 'REDIS',
       useFactory: () => {
-        // return new Redis('redis://red-cppadpqj1k6c73fvf120:6379');
-        return new Redis({
-          host: 'redis',
-          port: 6379,
-        });
+        return new Redis('redis://red-cppadpqj1k6c73fvf120:6379');
+        // return new Redis({
+        //   host: 'redis',
+        //   port: 6379,
+        // });
       },
     },
   ],
